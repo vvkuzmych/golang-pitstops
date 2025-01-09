03703d1a4a95d1516c995a704628180561ea475d
VKuzmich <vjk1976@ukr.net>
2025-01-09 21:43:42 +0200

    fix failed to build files

--- cmd/interfaces/05_conversion-vs-assertion/02_assertion/05_interface-int_mistmatched-types-error/main.go
+++ cmd/interfaces/05_conversion-vs-assertion/02_assertion/05_interface-int_mistmatched-types-error/main.go
@@ -4,5 +4,12 @@ import "fmt"
 
 func main() {
 	var val interface{} = 7
-	fmt.Println(val + 6)
+
+	// Type assertion to extract the integer value
+	intVal, ok := val.(int)
+	if ok {
+		fmt.Println(intVal + 6)
+	} else {
+		fmt.Println("val is not an integer")
+	}
 }
--- cmd/interfaces/05_conversion-vs-assertion/02_assertion/08_interface-cast-error_need-type-assertion/main.go
+++ cmd/interfaces/05_conversion-vs-assertion/02_assertion/08_interface-cast-error_need-type-assertion/main.go
@@ -4,11 +4,16 @@ import "fmt"
 
 func main() {
 	rem := 7.24
-	fmt.Printf("%T\n", rem)
-	fmt.Printf("%T\n", int(rem))
+	fmt.Printf("%T\n", rem)           // float64
+	fmt.Printf("%T\n", int(rem))      // int
 
 	var val interface{} = 7
-	fmt.Printf("%T\n", val)
-	fmt.Printf("%T\n", int(val))
-	//	fmt.Printf("%T\n", val.(int))
+	fmt.Printf("%T\n", val)           // interface{} (underlying type int)
+
+	// Type assertion to extract the integer value
+	if intVal, ok := val.(int); ok {
+		fmt.Printf("%T\n", intVal)    // int
+	} else {
+		fmt.Println("val is not an int")
+	}
 }
